9d295f4966ca0d70441f189881fd187430c32d8d
Mahaveer Rulaniya <65475928+mahaveer-rulaniya@users.noreply.github.com>
2021-06-20 13:00:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
         int_features = [int ( x ) for x in request.form.values ()]
         final_features = [np.array ( int_features )]
         prediction = model.predict ( final_features )
-        output = prediction[0]
+        output = prediction
 
         return render_template ( 'result.html', prediction_text=' The Recommended Crop to grow is {}'.format ( output ) )
 
@@ -33,7 +33,7 @@ def predict_api():
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
+    output = prediction
     return jsonify(output)
 
 if __name__ == "__main__":
