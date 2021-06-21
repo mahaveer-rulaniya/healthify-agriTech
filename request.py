7af2003772aaabdda53382447291d507999f4b30
Mahaveer Rulaniya <65475928+mahaveer-rulaniya@users.noreply.github.com>
2021-06-21 09:32:54 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'Temparature':60, 'Humidity':50, 'Moisture':40,'Nitrogen':20, 'Potassium':80,'Phosphorous':70,'Soil-Type':'Soil Type_Loamy','Crop-Type':'Crop Type_Sugarcane'})
+r = requests.post(url,json={'Temparature':30, 'Humidity':60, 'Moisture':40,'Nitrogen':20, 'Phosphorous':70,'Soil-Type':'Soil Type_Clayey','Crop-Type':'Crop Type_Pulses'})
 
 print(r.json())
